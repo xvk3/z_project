9e4bd57066ac4002b46848afa3037395fe99312d
w <w@xvk3.net>
2021-08-23 15:21:41 +0100

    converts to class adds pretty version

---
+++ scripts/util.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+
+class Util:
+  def utilHash(self, s):
+    rax = 5381
+    for c in s:
+      rdx = rax
+      rax = rax << 5
+      rax = rax + rdx
+      al = (rax & 0xff) ^ ord(c) 
+      rax = (rax & 0xffffffffffffff00) | al
+    return rax
+
+  def utilHashPretty(self, s):
+    return format(self.utilHash(s), "x").zfill(16) + " : " + s
+
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    for s in range(1, len(sys.argv)):
+      print(format(utilHash(sys.argv[s]), "x").zfill(16) + " : " + sys.argv[s])
+
