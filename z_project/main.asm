e1a138b516d3f19fd98e8b9dd2d130c6de8ff5d3
w <w@xvk3.net>
2021-09-07 09:57:34 +0100

    testing bijection and console functions

--- z_project/main.asm
+++ z_project/main.asm
@@ -1,6 +1,13 @@
-mpep segment read execute
+mpep segment read write execute
 
-EXTERN funcCallFunctionByHash:proc
+include header.inc
+
+EXTERN funcLookupFunctionByHash:proc
+EXTERN utilRand:proc
+
+EXTERN utilEncode:proc
+
+EXTERN dbgInitialise:proc
 
 main proc
 
@@ -19,30 +26,83 @@ main proc
   mov r15, [r8+30h]
 
   ;GetCurrentProcessorNumber
-  mov r10, 11046ac467aef24fh     ;qwHash
-  mov r11, r15
-  call funcCallFunctionByHash   ;	
-  mov rdx, rax                  ; ProcessorNumber
-  and rdx, 00000111b
-  shl rdx, 08h
-  mov r14, rdx
+  mov rdx, r15                  ;ImageBaseAddress
+  mov rcx, 11046ac467aef24fh    ;qwHash
+  call funcLookupFunctionByHash ;
+  call rax                      ;GetCurrentProcessorNumber
+
   ;dwSize - (ProcessorNumber & 00000111b) << 08 
+  mov rdx, rax                  ;ProcessorNumber
+  and rdx, 00000111b            ;MAX(7)
+  shl rdx, 08h                  ;7h => 700h
+  mov r14, rdx                  ;dwSize
 
   ;VirtualAlloc
-  mov r10, 0be0d6d6a19fbbf49h		;utilHash(VirtualAlloc)
-  mov r11, r15
-  xor rcx, rcx                  ;lpAddress - NULL
-  mov r8, 3000h                 ;flAllocationTYpe - MEM_COMMIT | MEM_RESERVE - 3000h
+  mov rdx, r15                  ;ImageBaseAddress
+  mov rcx, 0be0d6d6a19fbbf49h   ;qwHash
+  call funcLookupFunctionByHash ;VirtualAlloc
   mov r9, 40h                   ;flProtect - PAGE_EXECUTE_READWRITE - 40h
-  call funcCallFunctionByHash		;
+  mov r8, 3000h                 ;flAllocationTYpe - MEM_COMMIT | MEM_RESERVE - 3000h
+  mov rdx, r14                  ;dwSize
+  xor rcx, rcx                  ;lpAddress - NULL
+  call rax                      ;VirtualAlloc
+  test rax, rax
+  jz _ExitProcess
+
+  ;update MainMemory->genKernel32Base
+  mov qword ptr[rax+genKernel32Base], r15
+  mov r15, rax
+
+  call dbgInitialise
   nop
 
-  ;ExitProcess
-  mov r10, 0bf82c4b790c612ceh		;utilHash(ExitProcess)
-  mov r11, r15
-  mov rcx, r14                  ;uExitCode
-  call funcCallFunctionByHash		;
-  nop
+  ;WriteConsoleA
+  mov rdx, qword ptr[mainMemoryBase+genKernel32Base]
+  mov rcx, 85caeb217199930eh    ;qwHash
+  call funcLookupFunctionByHash ;
+  sub rsp, 30h
+  mov qword ptr[rsp+20h], 00h   ;lpReserved
+  xor r9, r9                    ;lpNumberOfCharsWritten
+  mov r8, 26                    ;nNumberOfCharsToWrite
+  lea rdx, some_string          ;lpBuffer
+  mov rcx, qword ptr[mainMemoryBase+dbgOutputHandle]
+  call rax                      ;WriteConsoleA
+  add rsp, 30h
+
+  ;encode "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  lea rcx, some_string
+  mov rdx, 26
+  mov r8, 0ff73d309bc332201h
+  call utilEncode
+
+  ;WriteConsoleA
+  mov rdx, qword ptr[mainMemoryBase+genKernel32Base]
+  mov rcx, 85caeb217199930eh    ;qwHash
+  call funcLookupFunctionByHash ;
+  sub rsp, 30h
+  mov qword ptr[rsp+20h], 00h   ;lpReserved
+  xor r9, r9                    ;lpNumberOfCharsWritten
+  mov r8, 28                    ;nNumberOfCharsToWrite
+  lea rdx, some_string_nl       ;lpBuffer
+  mov rcx, qword ptr[mainMemoryBase+dbgOutputHandle]
+  call rax                      ;WriteConsoleA
+  add rsp, 30h
+
+  _ExitProcess:
+    mov r8, qword ptr gs:[60h]    ;PEB
+    mov r8, qword ptr[r8+18h]     ;PEB->Ldr (_PEB_LDR_DATA)
+    lea r8, qword ptr[r8+10h]     ;_PEB_LDR_DATA->InLoadOrderModuleList (LDR_DATA_TABLE_ENTRY)
+    mov r8, qword ptr[r8]         ;_LIST_ENTRY->Flink (_LIST_ENTRY)
+    mov r9, qword ptr[r8]         ;_LIST_ENTRY->Flink (_LIST_ENTRY)
+    mov r8, qword ptr[r9]         ;_LIST_ENTRY->Flink (_LIST_ENTRY)
+    mov rdx, qword ptr[r8+30h]    ;kernel32Base
+    mov rcx, 0bf82c4b790c612ceh
+    call funcLookupFunctionByHash
+    mov rcx, r14
+    call rax
+
+  some_string_nl db 0Dh, 0Ah
+  some_string db "ABCDEFGHIJKLMNOPQRSTUVWXYZ",0
 
 main endp
 
